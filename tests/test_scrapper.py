c0e1823a5f91874b62552451fced324f07212462
HanslettTheDev <HanslettTheDev@gmail.com>
2024-01-31 17:29:22 +0100

    feat: added a GH action script to install and run tests and lint; feat: added dummy new unit test
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>

---
+++ tests/test_scrapper.py
@@ -0,0 +1,9 @@
+import pytest
+
+from bs4 import BeautifulSoup
+from escrapper.scrapping_parser import ParsePage
+
+def test_dummy():
+    # this is a dummy test data
+    parser = ParsePage(html_soup=BeautifulSoup())
+    assert type(parser) == ParsePage
